e82140baa94af398f6323c07b58f5f96ef52ed0d
aditya <aditya.pandey.241103@gmail.com>
2024-06-26 22:37:05 +0530

    first slider

---
+++ src/app/slider/page.jsx
@@ -0,0 +1,12 @@
+import FirstSlider from "@/components/Slider/FirstSlider"
+
+
+const SliderPage = () => {
+    return (
+        <>
+            <FirstSlider />
+        </>
+    )
+}
+
+export default SliderPage
